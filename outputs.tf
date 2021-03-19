acf12d4c85ac2ccf68761f1835e1b6e4d80dd752
Paul Thrasher <pthrasher@hashicorp.com>
2021-03-18 18:29:00 -0700

    less outputs for now. it was too much to look at!

--- outputs.tf
+++ outputs.tf
@@ -67,127 +67,13 @@ output "a-map" {
   description = "The private integer of the main server instance."
 }
 
-output "an-ip-address" {
-  value       = "127.0.0.1"
-  description = "The private url of the local server instance."
-}
-
-output "a-consul-domain" {
-  value       = "redis.consul:6379"
-  description = "The consul url of the main db instance."
-}
-
-output "a-url" {
-  value       = "https://sub.domain.com:8080/path?query=val&2=1#h1"
-  description = "The private url of the main server instance."
-}
-
-output "an-ftp-url" {
-  value       = "ftp://sub.domain.com:8081/path?query=val&2=1"
-  description = "The private ftp url of the main server instance."
-}
-
-output "an-ssh-url" {
-  value       = "ssh://git@github.com:thrashr888/terraform-outputs.git"
-  description = "The private ssh url of the main server instance."
-}
-
-output "a-bash-command" {
-  value       = "gh repo clone thrashr888/terraform-random"
-  description = "The private command of the main server instance."
-}
-
 output "a-long-string" {
   value       = "The private integer of the main server instance is where you want to go when you have the most fun in every Terraform instance you can see in the world that you live in except for dogs because they don't run servers in the same place that humans do."
   description = "A long string is good for you."
-}
-
-output "a-long-unbroken-string" {
-  value       = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"
-  description = "A long, unbroken string is good for you."
-}
-
-output "a-big-int" {
-  value       = 1234567890
-  description = "A big int is good for you."
-}
-
-output "a-big-decimal" {
-  value       = 1234567890.0987654321
-  description = "A big decimal is good for you."
-}
-
-output "empty-string" {
-  value       = ""
-  description = "Empty string ''"
-}
-
-output "empty-number" {
-  value       = 0
-  description = "Empty 0"
-}
-
-output "empty-list" {
-  value       = []
-  description = "Empty []"
-}
-
-output "empty-map" {
-  value       = {}
-  description = "Empty {}"
-}
-
-output "a-token" {
-  value       = "233b604b-b92e-48c8-a253-5f11514e4b50"
-  description = "A token is good for you."
 }
 
 output "a-sensitive-value" {
   value       = "hopefully you cannot see me"
   description = "The sensitive string of the main server instance."
   sensitive   = true
-}
-
-output "a-sensitive-number" {
-  value       = 867.5309
-  description = "The sensitive number of the main server instance."
-  sensitive   = true
-}
-
-output "a-sensitive-list" {
-  value       = [867, 5309]
-  description = "The sensitive list of the main server instance."
-  sensitive   = true
-}
-
-output "a-sensitive-map" {
-  value       = { 867 : 5309 }
-  description = "The sensitive map of the main server instance."
-  sensitive   = true
-}
-
-output "html" {
-  value = "<b>BOLD TEXT</b>"
-}
-
-output "markdown" {
-  value = "*BOLD TEXT*"
-}
-
-output "html-script" {
-  value = "<script>alert('hacked');</script>"
-}
-
-output "javascript" {
-  value = "alert('hacked');"
-}
-
-output "a-long-list" {
-  value       = ["example", 1001, 1000.1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "DUPE The private integer of the main server instance is where you want to go when you have the most fun in every Terraform instance you can see in the world that you live in except for dogs because they don't run servers in the same place that humans do."]
-  description = "A long string is good for you."
-}
-
-output "a-long-string-and-title-how-long-do-you-think-these-things-can-get-before-it-breaks-something-i-dont-know-the-answer-do-you" {
-  value       = "DUPE The private integer of the main server instance is where you want to go when you have the most fun in every Terraform instance you can see in the world that you live in except for dogs because they don't run servers in the same place that humans do."
-  description = "A long string is good for you."
 }
