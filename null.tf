9826ee0399e35d61b5dce902b2b961812952dc2b
Paul Thrasher <pthrasher@hashicorp.com>
2021-05-30 14:57:06 -0700

    bump

--- null.tf
+++ null.tf
@@ -7,14 +7,14 @@ resource "null_resource" "env" {
 }
 
 resource "null_resource" "null0" {
-  count = 50
+  count = 20
   triggers = {
     value = "EXAMPLE"
   }
 }
 
 resource "null_resource" "null1" {
-  count = 50
+  count = 20
    triggers = {
     value = "EXAMPLE-2"
   }
